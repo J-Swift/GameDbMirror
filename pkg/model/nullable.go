4f1a576f57ba324269fb08903accfb6fe082a1b0
Jimmy Reichley <jimmyqpublik@gmail.com>
2022-05-23 22:52:28 -0400

    Add missing UnmarshalJSON methods

--- pkg/model/nullable.go
+++ pkg/model/nullable.go
@@ -15,15 +15,41 @@ func (ni *NullInt) MarshalJSON() ([]byte, error) {
 	}
 	return json.Marshal(ni.Int32)
 }
+func (ni *NullInt) UnmarshalJSON(data []byte) error {
+	var obj int32
+	if err := json.Unmarshal(data, &obj); err != nil {
+		return err
+	}
+	*ni = NullInt{obj, true}
+	return nil
+}
+
 func (nb *NullBool) MarshalJSON() ([]byte, error) {
 	if !nb.Valid {
 		return []byte("null"), nil
 	}
 	return json.Marshal(nb.Bool)
 }
+func (nb *NullBool) UnmarshalJSON(data []byte) error {
+	var obj bool
+	if err := json.Unmarshal(data, &obj); err != nil {
+		return err
+	}
+	*nb = NullBool{obj, true}
+	return nil
+}
+
 func (ns *NullString) MarshalJSON() ([]byte, error) {
 	if !ns.Valid {
 		return []byte("null"), nil
 	}
 	return json.Marshal(ns.String)
+}
+func (ns *NullString) UnmarshalJSON(data []byte) error {
+	var obj string
+	if err := json.Unmarshal(data, &obj); err != nil {
+		return err
+	}
+	*ns = NullString{obj, true}
+	return nil
 }
